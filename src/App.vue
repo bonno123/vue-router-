7f1272379ab88e977c8e5560b07931c877ee4ca4
Avik <banikavik3@gmail.com>
2022-04-13 20:11:38 +0530

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script>
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
+// import sourceData from '@/data.json'
+import TheNavigation from "./components/TheNavigation.vue";
+
+export default {
+  components: { TheNavigation },
+};
+</script>
+
+<template>
+  <div>
+    <the-navigation />
+    <div class="container">
+      <router-view></router-view>
+    </div>
+  </div>
+</template>
+
+<style></style>
---
+++ src/components/TheNavigation.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="nav">
+    <router-link id="logo" to="/">Travel App</router-link>
+    <router-link
+      v-for="destination in destinations"
+      :key="destination.id"
+      :to="{
+        name: 'destination.show',
+        params: { id: destination.id, slug: destination.slug },
+      }"
+    >
+      {{ destination.name }}
+    </router-link>
+  </div>
+</template>
+<script>
+import src_data from "@/data.json";
+
+export default {
+  data() {
+    return {
+      destinations: src_data.destinations,
+    };
+  },
+};
+</script>
+
+<style>
+#nav .vue-school-active-link {
+  color: rgb(255, 255, 255);
+  border-bottom: 1px solid;
+}
+</style>
